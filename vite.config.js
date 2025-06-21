452cd4cb5a8fc60dd5f157e69f22f59aa820de13
Tripti Kashyap <tripti23557@iiitd.ac.in>
2025-06-21 23:35:07 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,26 +1,10 @@
-// import { defineConfig } from "vite";
-// import react from "@vitejs/plugin-react";
-// import tailwindcss from "@tailwindcss/vite";
-// import path from "path";
-
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react(), tailwindcss()],
-//   resolve: {
-//     alias: {
-//       "@": path.resolve(__dirname, "./src"),
-//     },
-//   },
-// });
-
-
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: "/Portfolio/",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
@@ -28,3 +12,5 @@ export default defineConfig({
     },
   },
 });
+
+
