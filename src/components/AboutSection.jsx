df9205afe2ca1c9639af45ad15d140e619d8aa78
Tripti Kashyap <tripti23557@iiitd.ac.in>
2025-06-22 10:21:45 +0530

    Update AboutSection.jsx

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -32,7 +32,7 @@ export const AboutSection = () => {
               </a>
 
               <a
-                href="/projects/Tripti_Resume_IMP.pdf"
+                href="/public/projects/Tripti_Resume_IMP.pdf"
                 download="Tripti_Kashyap_Resume.pdf"
                 className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
               >
